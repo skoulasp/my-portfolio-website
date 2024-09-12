eb3e73e230f63b580e6ffd1805258571e65858e1
Petros Skoulas <skoulasp@gmail.com>
2024-09-12 05:09:59 +0300

    Fix minor CSS bug

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
             const newTitle = urlHash.replace(/-/g, " ").replace(/\b\w/g, (c) => c.toUpperCase());
             document.title = `Petros Skoulas | ${newTitle}`;
         } else {
-            document.title = `Petros Skoulas | Front-End Developer`;
+            document.title = `Petros Skoulas | Front-End Web Developer`;
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [window.location.hash]);
